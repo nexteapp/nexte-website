4df26d58e170bc012c31cdc663ec868a0c2d165b
nexteapp <nexteappe@gmail.com>
2024-12-27 17:07:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// You can add interactivity here if necessary. 
+// For example, handling navigation smooth scrolling:
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
